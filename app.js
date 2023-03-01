4f4d1f0e047249f0012e26adaf98ea9ec533e5bb
Javier Giron Lopez <javiergironlopezbpr@gmail.com>
2023-03-01 16:03:33 +0100

    listado ok

--- app.js
+++ app.js
@@ -33,15 +33,16 @@ function countSex() {
             sexM++;
         }
     }
+    console.log('CONTABILIZACIÓN DEL SEXO:')
     console.log()
-    console.log (`Hay ${sexH} hombres y ${sexM} mujeres de un total de ${countSex.length} empleados.`)
+    console.log (`Hay ${sexH} hombres y ${sexM} mujeres de un total de ${countSex.length} empleados.`);
+    console.log()
 };
 
 // Salario bruto total de empresa 1 = 453166,667
 function salarioBruto(empresa) {
     dataExcel = readExcel('datos_prueba_tecnica.xlsx');
     let emp = empresa;
-    console.log(empresa);
     let totalSalary = 0;
 
     for (let salary of dataExcel){
@@ -50,18 +51,34 @@ function salarioBruto(empresa) {
             totalSalary += iSalary;
         }
     }
-
+    console.log('SALARIO BRUTO ANUAL DE LA EMPRESA:')
     console.log()
     console.log (`El salario bruto anual de la empresa 1 (Equilibrha IT y CT2) es de ${totalSalary.toFixed(3)} €`);
+    console.log()
 };
 
-//Listado de empleados
+//Listado de empleados que cobren +28000 y pertenezcan empresa 2. Id, apellidos, salario y
 function listadoEmpleados() {
+    dataExcel = readExcel('datos_prueba_tecnica.xlsx');
+    
+    console.log();
+    console.log (`LISTA DE EMPLEADOS:`);
+    console.log();
+    
+    for (let id of dataExcel){
+        if(id['ID Empresa'] == 2 && id['salario bruto anual'] > 28000) {
 
-    console.log()
-    console.log (`HACER LISTADO EMPLEADOS.`)
-    console.log()
+        console.log(`Id empleado: ${id['id empleado']}, nombre: ${id['nombre']} ${id['apellido1']} ${id['apellido2']}, salario: ${id['salario bruto anual'].toFixed(3)} €, empresa: ${id['Nombre empresa']}.`)
+        }
+    };
+    
+    console.log();
 };
+
+console.log()
+console.log('RESULTADOS DE LA PRUEBA TÉCNICA:')
+console.log()
+
 countSex();
-salarioBruto(2);
+salarioBruto(1);
 listadoEmpleados();
